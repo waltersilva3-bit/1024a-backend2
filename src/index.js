6c0403b73e7a08427a31b4dc8cccfbe5446a4417
waltersilva3-bit <walter.silva3@estudante.ifms.edu.br>
2026-03-03 09:26:35 -0300

    Atualização no index.ts

---
+++ src/index.js
@@ -0,0 +1,19 @@
+     numero = 10;
+var palavra = "Algum texto";
+var bol = true;
+var x = 10;
+x = "Guilherme";
+var vetor = [1, 2, 3, 4, 5];
+vetor[2] = 10;
+vetor.push(6);
+console.log(vetor);
+// vetor = [5,6,7,8,9]
+// import express from 'express'
+// const app = express()
+// app.use(express.json())
+// app.get("/", (req, res) => {
+//     res.send("Olá Mundo!")
+// })
+// app.listen(8000, ()=>{
+//     console.log("Meu servidor está rodando na porta 8000");
+// })
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,83 @@
-import express from 'express'
+let numero:number = 10
+let palavra:string = "Algum texto"
+let bol: boolean = true
 
-const app = express()
-app.use(express.json())
+let x:number|string = 10
+x = "Guilherme"
 
-app.get("/", (req, res) => {
-    res.send("Olá Mundo!")
-})
+const vetor: number[] = [1,2,3,4,5]
+vetor[2] = 10
+vetor.push(6)
+console.log(vetor)
+// vetor = [5,6,7,8,9]
 
-app.listen(8000, ()=>{
-    console.log("Meu servidor está rodando na porta 8000");
-})
+// Vetor de string
+
+let vetorString:string[] = ["a","b","ab"]
+
+type Pessoa={
+    id?:number,
+    nome:string;
+    idade:number
+}
+const pessoa:Pessoa ={
+    id:1,
+    nome:"Guilherme",
+    idade:18
+}
+pessoa.nome ="Terezinho"
+
+
+const cloudVoadora = []
+
+const batataVoadora:undefined = undefined
+
+
+const func = (nome:string):string=>`${nome} Lindo :3`
+func("Tere")
+
+function somaVetor(v:number[]):number{
+    const soma = v.reduce((prev,curr)=>prev+curr,0)
+    return soma
+}
+//Corrija o typescript abaixo
+function somaVetor2(v:number[]):number{
+    let soma = 0
+    for (let i = 0; i < v.length; i++) {
+        soma = soma + v[i]!
+    }
+    return soma
+}
+
+//Faça uma função que receba um vetor e devolva a soma
+//dos valores pares.
+
+function somaPar(v:number[]):number{
+    let soma = 0
+    for (let i = 0; i < v.length; i++) {
+        if(v[i]!%2==0){
+            soma  = soma + v[i]!
+        }
+    }
+    return soma
+}
+console.log(`O valor da somapar é ${somaPar([1,2,3])}`)
+
+//Faça uma função que receba dois vetores e retorne
+//um novo vetor com todos os valores passados.
+//Exemplo func merge([123],[456]) retorna [1,2,3,4,5,6]
+
+
+
+// import express from 'express'
+
+// const app = express()
+// app.use(express.json())
+
+// app.get("/", (req, res) => {
+//     res.send("Olá Mundo!")
+// })
+
+// app.listen(8000, ()=>{
+//     console.log("Meu servidor está rodando na porta 8000");
+// })
