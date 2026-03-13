6fbbaffbfba66edd6814730d3018494548947089
waltersilva3-bit <walter.silva3@estudante.ifms.edu.br>
2026-03-13 12:44:25 -0300

    Resolução de atividade e explicação de Find

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
+// import type { promises } from "node:dns"
+//import { TIMEOUT } from "node:dns"
+
+// import { error } from "node:console"
+/*
 let numero:number = 10
 let palavra:string = "Algum texto"
 let bol: boolean = true
@@ -51,7 +56,7 @@ function somaVetor2(v:number[]):number{
 
 //Faça uma função que receba um vetor e devolva a soma
 //dos valores pares.
-
+/*
 function somaPar(v:number[]):number{
     let soma = 0
     for (let i = 0; i < v.length; i++) {
@@ -62,7 +67,7 @@ function somaPar(v:number[]):number{
     return soma
 }
 console.log(`O valor da somapar é ${somaPar([1,2,3])}`)
-
+*/
 //Faça uma função que receba dois vetores e retorne
 //um novo vetor com todos os valores passados.
 //Exemplo func merge([123],[456]) retorna [1,2,3,4,5,6]
@@ -81,3 +86,122 @@ console.log(`O valor da somapar é ${somaPar([1,2,3])}`)
 // app.listen(8000, ()=>{
 //     console.log("Meu servidor está rodando na porta 8000");
 // })
+
+
+/*
+ * Exercício 01 - cria um novo vetor com os valores do vetor original mais dois novos valores
+ * Nome da função - criaNovoVetor
+ * Crie uma função que retorne um novo vetor com os valores do vetor original mais dois novos valores
+ * @param {number[]} vetor Vetor de números
+ * @param {number} valor1 Primeiro valor a ser adicionado
+ * @param {number} valor2 Segundo valor a ser adicionado
+ * @returns {number[]} Retorna um novo vetor com os valores do vetor original mais dois novos valores
+ * @example
+ * criaNovoVetor([1, 2, 3], 4, 5) // [1, 2, 3, 4, 5]
+ * criaNovoVetor([1, 2, 3], 0, 0) // [1, 2, 3, 0, 0]
+ */ 
+
+//Início do seu código
+
+
+
+//Fim do seu código
+
+/*
+ * Exercício 02 - divisivelPor11
+ * Nome da função - divisivelPor11
+ * Crie uma função que retorna um array com os números divisíveis por 11 no intervalo
+ * @param {number} min Número mínimo
+ * @param {number} max Número máximo
+ * @returns {number[]} Retorna um array com os números divisíveis por 11 no intervalo
+ * @example
+ *  divisivelPor11(1, 100) // [11, 22, 33, 44, 55, 66, 77, 88, 99]
+ *  
+ * divisivelPor11(11, 110) // [11, 22, 33, 44, 55, 66, 77, 88, 99, 110]
+ */
+
+//Início do seu código
+/*
+ function divisivelPor11(min:number, max:number){
+    const vetorDivisiveis11:number[] = []
+    for (let i = min; i < max; i++) {
+        if (1%11===0) {
+            vetorDivisiveis11.push(i)
+        }
+    }
+    return vetorDivisiveis11
+ }
+
+//Fim do seu código
+
+//Início do seu código
+
+function criaNovoVetor(vetor:number[],valor1:number,valor2:number){
+    const v = vetor.map(x=>x)
+    v.push(valor1,valor2)
+    return v
+}
+const vetor2 = [1,2,3]
+console.log(criaNovoVetor(vetor2, 0, 0))
+console.log(vetor2)
+
+//Fim do seu código
+
+// Comteúdo de hoje   => Promisses
+
+//Assincrona?
+function minhaPromessa():Promise<string>{
+    const prom:Promise<string> = new Promise<string>((resolve,reject)=>{
+        setTimeout(()=>resolve("Tere Erro"),5000)
+    })
+    return prom
+}
+minhaPromessa()
+.then((valor)=>console.log(valor))
+.catch((erro)=>console.log(erro))
+console.log("Exectuei esse código")
+
+try{
+   const resultado = await minhaPromessa()
+   console.log(resultado)
+}
+catch(erro){
+    console.log(erro)
+}
+
+//Crie uma função que tenha 50% de chance de retornar "tere" após
+// 5 segundos ou a função devolve o valor null
+// function devolveTere()
+
+function devolveTere():Promise<string|null>{
+    const Devolve = new Promise<string>((resolve, reject)=>{
+
+        if(Math.random()>=0.5){
+            setTimeout(()=>console.log("TERE"), 5000)
+        }else{
+        return null
+    }
+    })
+    return Devolve
+}
+
+devolveTere()
+.then((valor)=>console.log(valor))
+.catch((erro)=>console.log(erro))
+console.log("Executei esse código") 
+*/
+
+// VETORES
+// MAP
+// FIND
+// SPREAD
+
+const vetor:{id:number,nome:string,idade:number}[] = [
+    {id:1,nome:"tere",idade:32},
+    {id:1,nome:"Marcelo",idade:67},
+    {id:1,nome:"Grande Pablo Joseph VIII",idade:15},
+];
+
+//FIND Achar alguem maior de 18 anos?
+console.log(vetor.find((x)=>x.idade===18))
+console.log(vetor.filter ((x)=>x.idade===18))
